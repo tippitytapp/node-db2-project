34b8b1fa83cf69ac5ecb1554b696c70f6f9edc08
Marc Tapp <marc.tapp@gmail.com>
2020-05-16 17:38:43 -0400

    going to try and deploy

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,27 @@
-// Update with your config settings.
+require('dotenv').config()
 
 module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
       filename: './data/cars.db3'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
     },
-    useNullAsDefault: true,
+    migrations: {
+      directory:'./data/migrations'
+    },
+    seeds:{
+      directory: "./data/seeds"
+    }
   }
+
 };
